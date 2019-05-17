381fe9c91896efe3fd12d9953e65fe09db17c49b
ryf1123 <1971418293@qq.com>
2019-05-17 09:34:33 +0800

    upload unfinished lex

---
+++ mylex.py
@@ -0,0 +1,14 @@
+# 编译原理大作业 SPL based on Python
+# 任宇凡 刘洪甫 邱兆林
+
+# This file is the lex parse part of the whole project
+
+import ply.lex as lex
+from reserved import reserved
+
+print(reserved)
+
+class mylexer:
+	pass
+
+
