73c810449fcbcc2ba374eb277cc450ae6dcd4f4c
ryf1123 <1971418293@qq.com>
2019-06-11 00:00:50 +0800

    Update if.pas

--- CPP/test_cases/if.pas
+++ CPP/test_cases/if.pas
@@ -3,6 +3,13 @@ var x, y: integer;
 begin
 	x := 2;
     y := 15;
+
+	if ( x > y ) then
+		x := 3
+	else y := 3;
+
+	x := 1000;
+
 	if ( (x < y) and (x > y - 3) or (x - y >= 5)) then
 		x := x + 1
     else x := x - 1;
