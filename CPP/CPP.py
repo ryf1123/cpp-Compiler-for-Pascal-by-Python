6b42e131004650ce8c7feff9267c78b831b6f311
ryf1123 <1971418293@qq.com>
2019-06-15 09:10:21 +0800

    CPP.py

--- CPP/CPP.py
+++ CPP/CPP.py
@@ -8,36 +8,27 @@ if __name__ == '__main__':
         f = open(sys.argv[1], "r")
         data = f.read()
         f.close()
-        
         print("[info] source code loaded")
         print("\n\n\n")
-        result = parser.parse(data, debug=0)
-        
+
+        result = parser.parse(data, debug=1)
         print("[info] parser done")
         print("\n\n\n")
+        scopes['main'] = table.scope()
+
+        for scope in scopes:
+            print(scopes[scope])
 
         tac.addLinenum()
         tac.display()
-        allocReg = AllocateRegister.AllocteRegister(table, tac)
-        
+        allocReg = AllocateRegister.AllocteRegister(scopes, tac)
         print("[info] register allocation done")
         print("\n\n\n")
-            
-        codegen = CodeGen.CodeGen(table, tac, allocReg)
+        codegen = CodeGen.CodeGen(scopes, tac, allocReg)
+
         print("\n\n\n")
         print("[info] code generation done")
         print("--------- This is the end of this program ---------")
 
     else:
-        while True:
-            try:
-                data = input('Type here > ')
-            except EOFError:
-                break
-            if data == "q" or data == "quit":
-                break
-            if not data:
-                continue
-
-            result = parser.parse(data, debug=1)
-            print(result)
+        print("[error] error in paramater's number")
--- CPP/result.asm
+++ CPP/result.asm
@@ -1,213 +1,105 @@
 
-# 0 LABEL gcd main.gcd None
-gcd:
-
-# 1 = Symbol(`_t000000`, boolean, var, 12) Symbol(`b`, integer, var, 4) 0
-lw $t0, -4($fp)
-lw $t1, -12($fp)
-li $t8, 0
-seq $t1, $t0, $t8
-
-# 2 BEQ _l000000 Symbol(`_t000000`, boolean, var, 12) False
-sw $t0, -4($fp)
-sw $t1, -12($fp)
-li $t8, 0
-beq $t1, $t8, _l000000
-
-# 3 + Symbol(`_return`, integer, var, 8, [('a', 'integer', False), ('b', 'integer', False)]) Symbol(`a`, integer, var, 0) 0
+# 0 + Symbol(`a`, integer, const, 0, 2) 2 0
 lw $t0, 0($fp)
-lw $t1, -8($fp)
-li $t8, 0
-add $t1, $t0, $t8
+addi $t0, $0, 2
 
-# 4 JMP _l000001 None None
-sw $t0, 0($fp)
-sw $t1, -8($fp)
-j _l000001
-
-# 5 LABEL _l000000 None None
-_l000000:
-
-# 6 MOD Symbol(`_t000001`, integer, var, 16) Symbol(`a`, integer, var, 0) Symbol(`b`, integer, var, 4)
-lw $t0, 0($fp)
+# 1 + Symbol(`flag`, boolean, const, 4, True) True 0
 lw $t1, -4($fp)
-lw $t2, -16($fp)
-div $t0, $t1
-mfhi $t2
-
-# 7 PARAM None Symbol(`b`, integer, var, 4) None
-sw $t1, -76($sp)
-
-# 8 PARAM None Symbol(`_t000001`, integer, var, 16) None
-sw $t2, -80($sp)
-
-# 9 CALL Symbol(`_t000002`, integer, var, 20) gcd main
+addi $t1, $0, 1
 sw $t0, 0($fp)
 sw $t1, -4($fp)
-sw $t2, -16($fp)
-# = parent's
-lw $t8, 76($fp)
-sw $t8, 0($sp)
-sw $fp, -4($sp)
-sw $ra, -8($sp)
-sw $8, -12($sp)
-sw $9, -16($sp)
-sw $10, -20($sp)
-sw $11, -24($sp)
-sw $12, -28($sp)
-sw $13, -32($sp)
-sw $14, -36($sp)
-sw $15, -40($sp)
-sw $16, -44($sp)
-sw $17, -48($sp)
-sw $18, -52($sp)
-sw $19, -56($sp)
-sw $20, -60($sp)
-sw $21, -64($sp)
-sw $22, -68($sp)
-sw $23, -72($sp)
-addi $fp $sp -76
-addi $sp $sp -100
-jal gcd
-lw $t0, -20($fp)
-move, $t0, $v0
-sw $t0, -20($fp)
 
-# 10 + Symbol(`_return`, integer, var, 8, [('a', 'integer', False), ('b', 'integer', False)]) Symbol(`_t000002`, integer, var, 20) 0
-lw $t0, -20($fp)
-lw $t1, -8($fp)
-li $t8, 0
-add $t1, $t0, $t8
-sw $t0, -20($fp)
-sw $t1, -8($fp)
-
-# 11 LABEL _l000001 None None
-_l000001:
-
-# 12 RETURN Symbol(`_return`, integer, var, 8, [('a', 'integer', False), ('b', 'integer', False)]) main.gcd None
-lw $v0, -8($fp)
-addi $sp, $sp, 100
-lw $fp, 72($fp)
-lw $8, -12($sp)
-lw $9, -16($sp)
-lw $10, -20($sp)
-lw $11, -24($sp)
-lw $12, -28($sp)
-lw $13, -32($sp)
-lw $14, -36($sp)
-lw $15, -40($sp)
-lw $16, -44($sp)
-lw $17, -48($sp)
-lw $18, -52($sp)
-lw $19, -56($sp)
-lw $20, -60($sp)
-lw $21, -64($sp)
-lw $22, -68($sp)
-lw $23, -72($sp)
-move $t8, $ra
-lw $ra, -8($sp)
-jr $t8
-
-# 13 LABEL main None None
+# 2 LABEL main None None
 main:
 move $fp, $sp
-addi $sp, $sp, -16
+addi $sp, $sp, -112
 
-# 14 PARAM None 9 None
-li $t8, 9
-sw $t8, -76($sp)
+# 3 + Symbol(`y`, integer, var, 12) 2 0
+lw $t0, -12($fp)
+addi $t0, $0, 2
 
-# 15 PARAM None 36 None
-li $t8, 36
-sw $t8, -80($sp)
+# 4 = Symbol(`_t000002`, boolean, var, 52) 1 1
+lw $t1, -52($fp)
+addi $t1, $0, 1
 
-# 16 CALL Symbol(`_t000000`, integer, var, 4) gcd main
-# = fp
-sw $fp, 0($sp)
-sw $fp, -4($sp)
-sw $ra, -8($sp)
-sw $8, -12($sp)
-sw $9, -16($sp)
-sw $10, -20($sp)
-sw $11, -24($sp)
-sw $12, -28($sp)
-sw $13, -32($sp)
-sw $14, -36($sp)
-sw $15, -40($sp)
-sw $16, -44($sp)
-sw $17, -48($sp)
-sw $18, -52($sp)
-sw $19, -56($sp)
-sw $20, -60($sp)
-sw $21, -64($sp)
-sw $22, -68($sp)
-sw $23, -72($sp)
-addi $fp $sp -76
-addi $sp $sp -100
-jal gcd
-lw $t0, -4($fp)
-move, $t0, $v0
-sw $t0, -4($fp)
-
-# 17 PARAM None 3 None
-li $t8, 3
-sw $t8, -76($sp)
-
-# 18 PARAM None 6 None
-li $t8, 6
-sw $t8, -80($sp)
-
-# 19 CALL Symbol(`_t000001`, integer, var, 8) gcd main
-# = fp
-sw $fp, 0($sp)
-sw $fp, -4($sp)
-sw $ra, -8($sp)
-sw $8, -12($sp)
-sw $9, -16($sp)
-sw $10, -20($sp)
-sw $11, -24($sp)
-sw $12, -28($sp)
-sw $13, -32($sp)
-sw $14, -36($sp)
-sw $15, -40($sp)
-sw $16, -44($sp)
-sw $17, -48($sp)
-sw $18, -52($sp)
-sw $19, -56($sp)
-sw $20, -60($sp)
-sw $21, -64($sp)
-sw $22, -68($sp)
-sw $23, -72($sp)
-addi $fp $sp -76
-addi $sp $sp -100
-jal gcd
-lw $t0, -8($fp)
-move, $t0, $v0
-sw $t0, -8($fp)
-
-# 20 * Symbol(`_t000002`, integer, var, 12) Symbol(`_t000000`, integer, var, 4) Symbol(`_t000001`, integer, var, 8)
-lw $t0, -4($fp)
-lw $t1, -8($fp)
-lw $t2, -12($fp)
-mul $t2, $t0, $t1
-
-# 21 + Symbol(`ans`, integer, var, 0) Symbol(`_t000002`, integer, var, 12) 0
-lw $t3, 0($fp)
+# 5 + Symbol(`x`, boolean, var, 8) Symbol(`_t000002`, boolean, var, 52) 0
+lw $t2, -8($fp)
 li $t8, 0
-add $t3, $t2, $t8
+add $t2, $t1, $t8
 
-# 22 PRINT None Symbol(`ans`, integer, var, 0) None
-li $v0, 1
-addi $a0, $t3, 0
-syscall
+# 6 AND Symbol(`_t000003`, boolean, var, 56) True True
+lw $t3, -56($fp)
+addi $t3, $0, 1
 
-# 23 PRINTLN None None None
-li $v0, 11
-addi $a0, $0, 32
-syscall
-sw $t0, -4($fp)
-sw $t1, -8($fp)
-sw $t2, -12($fp)
-sw $t3, 0($fp)
+# 7 AND Symbol(`_t000004`, boolean, var, 60) Symbol(`_t000003`, boolean, var, 56) True
+lw $t4, -60($fp)
+li $t8, 1
+and $t4, $t3, $t8
+
+# 8 NOT Symbol(`_t000005`, boolean, var, 64) Symbol(`flag`, boolean, const, 4, True) None
+lw $t5, -4($fp)
+lw $t6, -64($fp)
+not $t6, $t5
+
+# 9 AND Symbol(`_t000006`, boolean, var, 68) Symbol(`_t000004`, boolean, var, 60) Symbol(`_t000005`, boolean, var, 64)
+lw $t7, -68($fp)
+and $t7, $t4, $t6
+
+# 10 + Symbol(`q`, boolean, var, 16) Symbol(`_t000006`, boolean, var, 68) 0
+lw $s0, -16($fp)
+li $t8, 0
+add $s0, $t7, $t8
+
+# 11 + Symbol(`_t000007`, integer, var, 72) Symbol(`a`, integer, const, 0, 2) 13
+lw $s1, 0($fp)
+lw $s2, -72($fp)
+li $t8, 13
+add $s2, $s1, $t8
+
+# 12 DIV Symbol(`_t000008`, integer, var, 76) Symbol(`_t000007`, integer, var, 72) 5
+
+# 13 MOD Symbol(`_t000009`, integer, var, 80) Symbol(`_t000008`, integer, var, 76) 1
+lw $s3, -76($fp)
+lw $s4, -80($fp)
+li $t8, 1
+div $s3, $t8
+mfhi $s4
+
+# 14 + Symbol(`x`, boolean, var, 8) Symbol(`_t000009`, integer, var, 80) 0
+li $t8, 0
+add $t2, $s4, $t8
+
+# 15 STOREREF 1 peoples Symbol(`x`, boolean, var, 8)
+
+# 16 LOADREF Symbol(`_t000010`, people_arr, var, 84) peoples 0
+
+# 17 + Symbol(`x`, boolean, var, 8) Symbol(`_t000010`, people_arr, var, 84) 0
+lw $s5, -84($fp)
+li $t8, 0
+add $t2, $s5, $t8
+
+# 18 STOREREF 1 newton score
+
+# 19 LOADREF Symbol(`_t000011`, integer, var, 108) newton score
+
+# 20 + Symbol(`x`, boolean, var, 8) Symbol(`_t000011`, integer, var, 108) 0
+lw $s6, -108($fp)
+li $t8, 0
+add $t2, $s6, $t8
+sw $t0, -12($fp)
+sw $t1, -52($fp)
+sw $t2, -8($fp)
+sw $t3, -56($fp)
+sw $t4, -60($fp)
+sw $t5, -4($fp)
+sw $t6, -64($fp)
+sw $t7, -68($fp)
+sw $s0, -16($fp)
+sw $s1, 0($fp)
+sw $s2, -72($fp)
+sw $s3, -76($fp)
+sw $s4, -80($fp)
+sw $s5, -84($fp)
+sw $s6, -108($fp)
 li $v0, 10
 syscall
